0a6fd6c490b127ac73ae30ceec2e60b4fd7ea8f7
Sjlvascripter <silvascripter@gmail.com>
2024-08-11 15:35:46 -0300

    primeiro commit

---
+++ conversao_de_tipos.py
@@ -0,0 +1,25 @@
+preco = 10
+print (preco)
+
+preco = float(preco)
+print (preco)
+# Exemplo de conversão de tipo int para float 
+
+
+preco = 10.00
+print (preco)
+
+preco = int(preco)
+print (preco)
+# Exemplo de conversão de tipo float para int
+
+
+preco = 10
+print (preco)
+
+preco = 10 / 2
+print (preco)
+
+preco = 10 // 2
+print (preco)
+# No exemplo acima mostro a conversão de tipos usando a divisão, aonde podemos perceber que quando usamos apenas uma "/" (barra) para fazer a operação o valor obtido sai em formato de float e quando usamos duas "/" (barras) o valor obtido é em formato int.
